2082a42c3920e5be3629f4056c94b4d8fcb8b0c3
MustafaK99 <rmk799@outlook.com>
2023-03-16 18:00:58 +0000

    somewhat working markdown generator

--- app.py
+++ app.py
@@ -10,25 +10,18 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 @app.route("/", methods=("GET", "POST"))
 def index():
     if request.method == "POST":
-        animal = request.form["animal"]
+        code = request.form["code"]
         response = openai.Completion.create(
             model="text-davinci-003",
-            prompt=generate_prompt(animal),
-            temperature=0.6,
+            prompt=f"{code}\n\nRead the code and generate a README.md for it in markdown format",
+            temperature=1,
+            max_tokens=64,
+            top_p=1.0,
+            frequency_penalty=2.0,
+            presence_penalty=0.
         )
         return redirect(url_for("index", result=response.choices[0].text))
 
     result = request.args.get("result")
     return render_template("index.html", result=result)
 
-
-def generate_prompt(animal):
-    return """Suggest three names for an animal that is a superhero.
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: {}
-Names:""".format(
-        animal.capitalize()
-    )
