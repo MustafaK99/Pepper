515007700f10f20f40b60a35278652a612a19439
MustafaK99 <rmk799@outlook.com>
2023-03-17 17:21:54 +0000

    looks kinda alright need to fix formatting of this

--- app.py
+++ app.py
@@ -2,8 +2,10 @@ import os
 
 import openai
 from flask import Flask, redirect, render_template, request, url_for
+from flaskext.markdown import Markdown
 
 app = Flask(__name__)
+Markdown(app)
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 
@@ -20,7 +22,9 @@ def index():
             frequency_penalty=2.0,
             presence_penalty=0.
         )
-        return redirect(url_for("index", result=response.choices[0].text))
+
+        mkd_text = response.choices[0].text
+        return redirect(url_for("index", result=mkd_text))
 
     result = request.args.get("result")
     return render_template("index.html", result=result)
